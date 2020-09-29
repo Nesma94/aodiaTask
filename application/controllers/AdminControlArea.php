13e20cf3deb13c0dfcfcaa555a317db6390ef1b6
NesmaMohamed94 <nesmamohamed94@gmail.com>
2020-09-29 22:55:43 +0200

    upload folders

--- application/controllers/AdminControlArea.php
+++ application/controllers/AdminControlArea.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
@@ -18,25 +17,4 @@ class AdminControlArea extends CI_Controller
         $this->load->view('includes/footer.php');
     }
 
-=======
-<?php
-defined('BASEPATH') or exit('No direct script access allowed');
-
-class AdminControlArea extends CI_Controller
-{
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->helper('url');
-        $this->load->model('user_model');
-        $this->user_model->verfiyLogin();
-    }
-    public function index()
-    {
-        $this->load->view('includes/header.php');
-        $this->load->view('panel/admin.php');
-        $this->load->view('includes/footer.php');
-    }
-
->>>>>>> fdd7924d756c5da9cfce16ee7b255e5384e853ef
 }
