67099d0ffbed3053be95408c7192eed27ae7adbf
NesmaMohamed94 <nesmamohamed94@gmail.com>
2020-09-29 22:59:19 +0200

    upload task folders

--- application/views/panel/addAccount.php
+++ application/views/panel/addAccount.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <div class="content-wrapper">
 	<div class="container-fluid" style="margin-top: 60px;">
 		<div class="row">
@@ -51,57 +50,3 @@
 			</div>
 		</div>
 	</div>
-=======
-<div class="content-wrapper">
-	<div class="container-fluid" style="margin-top: 60px;">
-		<div class="row">
-			<div class="col-12">
-				<h5 class="mt-3">Add Account</h5>
-				<hr class="my-5">
-				<!-- Form -->
-				<form enctype="multipart/form-data" action="<?= base_url() ?>account/doAddAccount" method="POST">
-					<div class="form-group row">
-						<label class="col-sm-2 col-form-label">Username
-							<span class="text-danger">*</span>
-						</label>
-						<div class="col-sm-4">
-							<input required name="name" type="text" class="form-control">
-						</div>
-					</div>
-					<div class="form-group row">
-						<label class="col-sm-2 col-form-label">Email Address
-							<span class="text-danger">*</span>
-						</label>
-						<div class="col-sm-4">
-							<input required name="email" type="email" class="form-control">
-						</div>
-					</div>
-					<div class="form-group row">
-						<label class="col-sm-2 col-form-label">Role
-							<span class="text-danger">*</span>
-						</label>
-						<div class="col-sm-4">
-							<select name="role" id="" class="form-control" required="">
-								<option value="admin">Admin</option>
-								<option value="superAdmin">Super Admin</option>
-							</select>
-						</div>
-					</div>
-					<div class="form-group row">
-						<label class="col-sm-2 col-form-label">Password
-							<span class="text-danger">*</span>
-						</label>
-						<div class="col-sm-4">
-							<input required name="password" type="password" class="form-control">
-						</div>
-					</div>
-					<div class="form-group row py-5">
-						<div class="col-sm-4">
-							<button type="submit" class="btn btn-primary rounded-0">Add New Account</button>
-						</div>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
->>>>>>> fdd7924d756c5da9cfce16ee7b255e5384e853ef
--- application/views/panel/viewDriver.php
+++ application/views/panel/viewDriver.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <div class="content-wrapper">
     <div class="container-fluid" >
         <h3 class="my-3">Drivers</h3>
@@ -67,74 +66,4 @@
                 window.location = "<?= base_url() ?>Driver/deleteDriver/" + x;
             }
         }
-=======
-<div class="content-wrapper">
-    <div class="container-fluid" >
-        <h3 class="my-3">Drivers</h3>
-          <a href="<?= base_url() ?>Driver/addDriver" class="btn btn-primary rounded-0">Add Driver</a>
-        <hr class="my-4">
-        <?php if ($this->session->flashdata('true')) { ?>
-        <div class="alert alert-success text-center rounded-0 border-success" role="alert">
-            <span class="font-weight-bold">
-                <i class="fa fa-check-circle-o pr-1"></i>
-                <?= $this->session->flashdata('true') ?>
-            </span>
-        </div>
-        <?php
-
-    } ?>
-        <?php if ($this->session->flashdata('error')) { ?>
-        <div class="alert alert-danger text-center rounded-0 border-danger" role="alert">
-            <span class="font-weight-bold">
-                <i class="fa fa-times-circle-o pr-1"></i>
-                <?= $this->session->flashdata('error') ?>
-            </span>
-        </div>
-        <?php
-
-    } ?>
-        <!-- Example DataTables Card-->
-        <div class="table-responsive">
-            <table class="table table-bordered table-striped" width="100%" cellspacing="0"  id="dataTable">
-                <thead>
-                    <tr>
-                        <!-- Changed from ID to Order -->
-                        <th>Name</th>
-                        <th>Team</th>
-                        <th>Edit</th>
-                        <th>Delete</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($drivers as $driver) { ?>
-                    <tr>
-                        <td>
-                            <?= $driver->name ?>
-                        </td>
-                        <td>
-                            <?= $driver->team_name ?>
-                        </td>
-                     
-                        <td>
-                            <a class="btn btn-warning btn-block rounded-0" href="<?= base_url() ?>driver/editDriver/<?= $driver->id ?>">Edit</a>
-                        </td>
-                        <td>
-                            <a class="btn btn-danger btn-block rounded-0" onclick="delete_driver(<?= $driver->id ?>)">Delete</a>
-                        </td>
-                    </tr>
-                    <?php
-
-                } ?>
-                </tbody>
-            </table>
-        </div>
-    </div>
-      <script type="text/javascript">
-        function delete_driver(x) {
-            var r = confirm(" Do You Really Want To Delete This Driver !!");
-            if (r == true) {
-                window.location = "<?= base_url() ?>Driver/deleteDriver/" + x;
-            }
-        }
->>>>>>> fdd7924d756c5da9cfce16ee7b255e5384e853ef
     </script>
